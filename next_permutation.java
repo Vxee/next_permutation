bc4f61bced88c2f281a6756fd903290102d915c2
Vxee <15258691200@163.com>
2017-04-04 19:04:25 +0800

    Update next_permutation.java

--- next_permutation.java
+++ next_permutation.java
@@ -1,22 +1,20 @@
-class Solution {
-public:
-    void nextPermutation(vector<int> &num) {
-		if (num.size() <= 1) return;
+public void nextPermutation(vector<int> &num) 
+{
+	if (num.size() <= 1) return;
         
-		int i = (int)num.size() - 1;
-		while (i > 0 && num[i] <= num[i-1])
-			--i;
-		if (i == 0) {
-			reverse(num.begin(), num.end());
-			return;
-		}
-		
+	int i = (int)num.size() - 1;
+	while (i > 0 && num[i] <= num[i-1])
 		--i;
-		int j = (int)num.size() - 1;
-		while (!(num[i] < num[j])) 
-			--j;
+	if (i == 0) {		//这是一个递减的序列
+		reverse(num.begin(), num.end());
+		return;
+	}
 		
-		swap(num[i], num[j]);
-		reverse(num.begin() + i + 1, num.end());
-    }
-};
+	--i;
+	int j = (int)num.size() - 1;
+	while (!(num[i] < num[j])) 
+		--j;
+		
+	swap(num[i], num[j]);
+	reverse(num.begin() + i + 1, num.end());
+}
