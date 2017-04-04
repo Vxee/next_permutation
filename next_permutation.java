e36265bc726d7de5a7b9763ce747cccae0a71b12
Vxee <15258691200@163.com>
2017-04-04 19:25:22 +0800

    Update next_permutation.java

--- next_permutation.java
+++ next_permutation.java
@@ -1,20 +1,57 @@
-public void nextPermutation(vector<int> &num) 
-{
-	if (num.size() <= 1) return;
-        
-	int i = (int)num.size() - 1;
-	while (i > 0 && num[i] <= num[i-1])
-		--i;
-	if (i == 0) {		//这是一个递减的序列
-		reverse(num.begin(), num.end());
-		return;
+import java.util.Scanner;
+public class Main {
+	
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="12345";
+		int[] a  = new int[]{1,2,3,4,5};
+		nextPermutation(a);
+		for(int i=0;i<5;i++)
+		{
+			System.out.print(a[i]);
+		}
+		
 	}
-		
-	--i;
-	int j = (int)num.size() - 1;
-	while (!(num[i] < num[j])) 
-		--j;
-		
-	swap(num[i], num[j]);
-	reverse(num.begin() + i + 1, num.end());
+	
+	public static void nextPermutation(int[] num) 
+	{
+		if (num.length <= 1) return;
+	        
+		int i = num.length - 1;
+		while (i > 0 && num[i] <= num[i-1])
+			--i;
+		if (i == 0) {		//这是一个递减的序列
+			reverse(0, num.length-1,num);
+			return;
+		}
+			
+		--i;
+		int j = num.length - 1;
+		while (!(num[i] < num[j])) 
+			--j;
+			
+		swap(i,j,num);
+		reverse( i + 1, num.length-1,num);
+	}
+	
+	private static void swap(int i, int j,int[] a) {
+		// TODO Auto-generated method stub
+		int index=a[i];
+		a[i]=a[j];
+		a[j]=index;
+	}
+
+	public static void reverse(int start,int end,int[] a)
+	{
+		int index,i,j;
+		for(i=start,j=end;i<=(start+end)/2;i++,j--)
+		{
+			index=a[i];
+			a[i]=a[j];
+			a[j]=index;
+		}
+	}
+	
+	
 }
